7124e0f57ad566ea79bc48d6c06e62c1b58cd0ff
tofutigerz <hfu3@ualberta.ca>
2022-05-15 07:44:48 -0600

    onfocusout, editable creates dialog

--- Components/Editable.jsx
+++ Components/Editable.jsx
@@ -1,20 +1,51 @@
+import { Button, Dialog, DialogTitle } from "@mui/material";
 import React, { useState } from "react";
 
 function Editable(props) {
-	const [clicked, setClicked] = useState(false);
-
 	const { prefix, suffix, content } = props;
+	const [clicked, setClicked] = useState(false);
+	const [editedContent, setEditedContent] = useState(content);
+	const [open, setOpen] = useState(false);
+
+	const handleChange = e => {
+		setEditedContent(e.target.value);
+	};
+
+	const handleFocusOut = e => {
+		setOpen(true);
+		setClicked(false);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleUpdate = () => {};
+
 	return (
 		<>
 			{clicked && (
 				<>
 					<label htmlFor="editable">{prefix}</label>
-					<input type={typeof content} value={content} name="editable"></input>
+					<input
+						autoFocus
+						onBlur={handleFocusOut}
+						type={typeof content}
+						value={editedContent}
+						name="editable"
+						onChange={handleChange}
+					></input>
 				</>
 			)}
 			{!clicked && (
 				<p onClick={() => setClicked(true)}>{prefix + content + suffix}</p>
 			)}
+
+			<Dialog open={open}>
+				<DialogTitle>Would you like to update your preferences?</DialogTitle>
+				<Button onClick={handleUpdate}>Yes</Button>
+				<Button onClick={handleClose}>No</Button>
+			</Dialog>
 		</>
 	);
 }
